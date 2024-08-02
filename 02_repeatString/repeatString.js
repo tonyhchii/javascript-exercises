6213af48655601b1153bae07d213086506eac7bd
Tony Huang <tonyhchii@gmail.com>
2024-08-02 14:10:26 -0400

    Completed 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedString, amount) {
+    let newString = "";
+    if (amount < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < amount; i++) {
+        newString += repeatedString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
