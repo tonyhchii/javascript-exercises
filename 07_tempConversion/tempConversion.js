eef2ca615bb905e5481e17f17084e7ea71eb605a
Tony Huang <tonyhchii@gmail.com>
2024-08-02 15:08:34 -0400

    completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const fahToCelRatio = 5/9;
+  const conversionConstant = 32;
+  let celsius = fahToCelRatio * (temperature - conversionConstant);
+  return Math.round(celsius * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const celToFahRatio = 9/5;
+  const conversionConstant = 32;
+  let fahrenheit = celToFahRatio * temperature + conversionConstant;
+  return Math.round(fahrenheit * 10)/10;
 };
 
 // Do not edit below this line
