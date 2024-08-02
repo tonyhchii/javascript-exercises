6e7143280fa91257cb3ddef83d37482aec799b57
Tony Huang <tonyhchii@gmail.com>
2024-08-02 14:50:55 -0400

    Completed 05 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    if(!Number.isInteger(begin) || !Number.isInteger(end) || begin < 0 || end < 0){
+        return "ERROR"
+    }
+    let sum = 0;
+    if (begin > end) {
+        let temp = begin;
+        begin = end;
+        end = temp;
+    }
+    for(let i = begin; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
