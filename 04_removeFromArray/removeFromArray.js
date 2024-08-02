b2c9cca11d18e365d464cfa64e3160181e49f12d
Tony Huang <tonyhchii@gmail.com>
2024-08-02 14:42:27 -0400

    removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...number) {
+    for (let i = 0; i < number.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            while(arr[j] === number[i]) {
+                arr.splice(j,1);
+            }
+        }
+     }
+     console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
