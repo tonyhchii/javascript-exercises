c5ee3764be611af8c661231cdf58b4a58acce612
Tony Huang <tonyhchii@gmail.com>
2024-08-04 13:06:59 -0400

    finished calculator 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, next) => total + next, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, next) => total * next);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return b == 0 ? 1 : power(a, b-1) * a;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  return a == 0 ? 1 : factorial(a - 1) * a;
 };
 
 // Do not edit below this line
